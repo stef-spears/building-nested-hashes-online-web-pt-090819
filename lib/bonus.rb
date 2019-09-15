3e6ccbe02f7045b544343fcfcaf40624547666d4
Stef Spears <spears.sj87@gmail.com>
2019-09-15 19:17:18 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,4 @@
+=begin
 def bonus
   epic_tragedy = {
    :montague => {
@@ -28,3 +29,5 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+=end
